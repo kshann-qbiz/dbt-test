8ef89a66350d54bd14233aa29598418c57302c3d
Kelley <kelleyshann@gmail.com>
2025-06-17 15:05:55 -0400

    added new models

--- models/stg_species.sql
+++ models/stg_species.sql
@@ -11,17 +11,9 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+select * from qbiz_data.raw.animal_species
 
 )
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
